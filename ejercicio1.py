810de02f69bc91b519e4a325b99f690f5fb586d6
Nicolas Moreno <nicolas.morenob@upb.edu.co>
2026-02-27 15:11:33 -0500

    se realizaron los ejercicios de operadores

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+cuenta = 85
+porcentaje_propina =0.15
+personas = 4
+
+propina = cuenta * porcentaje_propina
+total_pagar = cuenta + propina 
+pago_por_persona = total_pagar / personas 
+
+print("Total de la propina", propina)
+print("Total a pagar",total_pagar)
+print("Cada persona debe pagar", pago_por_persona)
